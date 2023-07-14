eca5154238bcda9d7c28fb307af30704bef2f1cf
sla-zy <mwangikadavid@gmail.com>
2023-07-14 13:36:28 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - aprogramm that print a line with puts function
+ * Return: 0 (correct)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
