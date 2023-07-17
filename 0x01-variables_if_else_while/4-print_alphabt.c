43a634e54ad151d2c774ab773947cf542d9a56cf
sla-zy <mwangikadavid@gmail.com>
2023-07-16 17:11:40 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print alphabets exept two
+ * Return: Always 0
+ */
+
+int main(void)
+{
+        char x;
+
+        x = 'a';
+
+        while (x <= 'z')
+	{
+	        if (x != 'e' && x != 'q')
+		{
+		     	putchar(x);
+		}
+	}	x++;
+	putchar('\n');
+        return (0);
+}
+
