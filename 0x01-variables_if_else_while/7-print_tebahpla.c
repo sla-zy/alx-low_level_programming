346212f2d548ae20103651c7b995d6e62293f3a6
sla-zy <mwangikadavid@gmail.com>
2023-07-17 13:19:38 -0700

    iovely

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet in revese,followe by new line
+ * Return: Always 0
+ */
+int main (void)
+{
+	char y;
+	
+	for (y = 'z'; y >= 'a'; y--)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
