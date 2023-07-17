6978c97bc9e749b9f1d0775a0a4a6231896a6e8b
sla-zy <mwangikadavid@gmail.com>
2023-07-17 12:32:32 -0700

    lit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Description: 'print all single digits numbers of base 10 starting from 0, followed by a new line'
+ * Return: Always 0 (correct)
+ */
+int main (void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+
+}
+	  
+
