4acf0ec53c1d2d237f7393124386d7a752772d60
sla-zy <mwangikadavid@gmail.com>
2023-09-10 03:33:38 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,8 @@ int main(void)
 		printf("last digit of %d is %d and is greater than 5\n", n, g);
 	else if (g < 6 && g != 0)
 		printf("last digit of %d is %d and is 0\n", n, g);
-	else 
+	else
 		printf("last digit of %d is %d  and is less than 6 and not 0\n", n, g);
+
 	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - print alphabets exept two
- * Return: Always 0
+ * Return: Always 0 (correct)
  */
 
 int main(void)
@@ -15,9 +15,9 @@ int main(void)
 	        if (x != 'e' && x != 'q')
 		{
 		     	putchar(x);
+			x++
 		}
-	}	x++;
+	}
 	putchar('\n');
         return (0);
 }
-
